0b7c433bd305fbde08ea0a663a64ab322a3c20ef
keicoon <keicoon15@gmail.com>
2018-11-28 15:08:43 +0900

    add python post_processing

--- test/index.js
+++ test/index.js
@@ -1,17 +1,17 @@
 let ArgumentParser = require('argparse').ArgumentParser;
 let parser = new ArgumentParser({
   version: '0.0.1',
-  addHelp:true,
+  addHelp: true,
   description: 'GetYourFaceVideoData'
 });
 parser.addArgument(
-  [ '-c', '--channel' ],
+  ['-c', '--channel'],
   {
     help: 'UChlgI3UHCOnwUGzWzbJ3H5w'
   }
 );
 parser.addArgument(
-  [ '-n', '--num' ],
+  ['-n', '--num'],
   {
     type: 'int',
     help: 500
@@ -19,20 +19,24 @@ parser.addArgument(
 );
 let args = parser.parseArgs();
 
-async function main () {
-    const GYFVD = require('../');
-    const gyfvd = new GYFVD({
-        "log_level": "trace", //"info"
-        "subtitle_download": true,
-        "subtitle_validation": true
-    });
+async function main() {
+  const GYFVD = require('../');
+  const gyfvd = new GYFVD({
+    "log_level": "trace", //"info"
+    "subtitle_download": true,
+    "subtitle_validation": true,
+    "py_process": [{
+      "path_script": "../temp_jun/utils/split_mp4.py",
+      "args": ["-d=./db/result/","--split_by_sentence"]
+    }]
+  });
 
-    await gyfvd.start(args.channel, args.num);
+  await gyfvd.start(args.channel, args.num);
 
-    // await gyfvd.crawl(args.channel, args.num);
-    // await gyfvd.data_process(args.num);
+  // await gyfvd.crawl(args.channel, args.num);
+  // await gyfvd.data_process(args.num);
 
-    await gyfvd.clear();
+  await gyfvd.clear();
 }
 
 main();
