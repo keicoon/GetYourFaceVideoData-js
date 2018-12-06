2703a4ac56de3834af054327da5b36e1c46e14df
keicoon15 <keicoon15@gmail.com>
2018-12-06 17:34:36 +0900

    modify test script

--- test/valid_video_data.js
+++ test/valid_video_data.js
@@ -5,10 +5,11 @@ const db = require('../lib/db');
 const { PATH_VIDEO, PATH_SUBTITLE } = db;
 const files = fs.readdirSync(PATH_VIDEO);
 
-const mode = process.argv[2];
+const mode = process.argv[2] || 'empty';
+const min_video_size = process.argv[3] || 100;
 let list = db.get_db();
 
-console.log('working mode is', mode);
+console.log('working mode is', mode, 'size is', min_video_size);
 if (mode == 'empty') {
     let find_files = [];
     files.forEach(file_name => {
@@ -43,17 +44,20 @@ if (mode == 'empty') {
     })
     console.log('aftr db length:', Object.keys(new_list).length);
     fs.writeFileSync(db.PATB_DB_LIST, JSON.stringify(new_list, null, 4), 'utf8');
-} else if (mode == 'size100') {
+} else if (mode == 'size') {
     let find_paths = [];
     files.forEach(file_name => {
         const cur_full_path = path.resolve(PATH_VIDEO, file_name)
         const stat = fs.statSync(cur_full_path)
         if (stat.isFile()) {
-            if (stat.size < 100) {
+            if (stat.size < min_video_size) {
                 find_paths.push(cur_full_path)
             }
         }
     })
+    
+    if(find_paths.length < 1) return console.log('cant find satisfy target videos');
+    
     console.log('prev db length:', Object.keys(list).length);
     find_paths.forEach(file_path => {
         console.log('remove invalid video data', file_path);
